c5453ec9be86e7b028ad5795ae797bea0cc635a7
dandan <1076881083@qq.com>
2017-11-19 17:04:54 +0800

    列表页的分页

--- src/api/list.php
+++ src/api/list.php
@@ -3,11 +3,15 @@
  * @Author: Marte
  * @Date:   2017-11-17 15:05:37
  * @Last Modified by:   Marte
- * @Last Modified time: 2017-11-17 15:06:22
+ * @Last Modified time: 2017-11-19 16:01:23
  */
     include 'connect.php';
 
-    $sql = "select * from goods";
+    $pageNo = isset($_GET['pageNo']) ? $_GET['pageNo'] : 1;
+    $qty = isset($_GET['qty']) ? $_GET['qty'] : 50;
+
+
+    $sql = "select * from goods limit ".(($pageNo-1)*$qty).",".$qty;
 
     // 获取结果集
     $result = $conn->query($sql);
@@ -17,8 +21,23 @@
     $row = $result->fetch_all(MYSQLI_ASSOC);
     
     // 把数组转换成json字符串
-    $res = json_encode($row,JSON_UNESCAPED_UNICODE);
+    // $res = json_encode($row,JSON_UNESCAPED_UNICODE);
 
-    echo "$res";
+    // echo "$res";
+
+    // 获取总数据
+    $total_sql = "select count(*) from goods";
+    // 获取数据总条数
+    $total_result = $conn->query($total_sql);
+    $total_res = $total_result->fetch_row();
+    // 生成总页数
+    $total_page = ceil($total_res[0]/50);
+    // echo "$total_page";
+
+    $total_data = array(
+        'data' => $row,
+        'total_page'=>$total_page
+        );
+    echo json_encode($total_data,JSON_UNESCAPED_UNICODE);
 
 ?>
--- src/js/list.js
+++ src/js/list.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-11-17 15:05:24
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-11-18 16:46:05
+* @Last Modified time: 2017-11-19 17:02:41
 */
 
 require.config({
@@ -13,153 +13,186 @@ require.config({
 
 require(['common','ajax','jquery'],function(com,oAjax,$){
 
+    var pageNo = 1;
+    var qty = 50;
+
    // 链接右边栏
     $('.right_lan').load('../html/goods.html .right-slidebar');
-    ajax({
-            type:'get',
-            url:'http://localhost:1111/api/list.php',
-            async:true,
-            success:function(data){
 
-                var ul = document.createElement('ul');
-                var prolist = $('.prolist').get(0);
-                ul.className = 'pro-class';
-                ul.innerHTML = data.map(function(item){                  
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(xhr.status == 304 || xhr.status == 200){
+            var data = JSON.parse(xhr.responseText);
+            // 生成页面数据
+            var ul = document.createElement('ul');
+            var prolist = $('.prolist').get(0);
+            ul.className = 'pro-class clearfix';
+            ul.innerHTML = data.data.map(function(item){                  
 
-                    // 判断是不是热销
-                    if(item.ishot == 0){
-                        // 判断右上角小图标是哪个
-                        if(item.pro_icon_while_one == 0){
-                            return `
-                                <li data-id="${item.id}">
-                                    <div class="pro-show">
-                                        <div class="pro-icon"><img src="../img/icon-bl-3.png" alt="" /></div>
-                                        <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
-                                        <div class="pro-money">
-                                            <div class="money-fl">￥${item.price}</div>
-                                        </div>
-                                        <div class="product-comment">
-                                            <div class="pro-name"><a></a></div>
-                                            <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-left" style="cursor:pointer;">${item.pinjia_num}</div>
-                                            <div class="pro-assess-new" style="cursor:pointer;">${item.dianzanlv}</div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-right">
-                                                <a>${item.business_category}</a>
-                                            </div>
-                                        </div>
-                                        <div class="pro-button">
-                                            <button class="btn btn-primary addCard">加入购物车</button>
+                // 判断是不是热销
+                if(item.ishot == 0){
+                    // 判断右上角小图标是哪个
+                    if(item.pro_icon_while_one == 0){
+                        return `
+                            <li data-id="${item.id}">
+                                <div class="pro-show">
+                                    <div class="pro-icon"><img src="../img/icon-bl-3.png" alt="" /></div>
+                                    <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
+                                    <div class="pro-money">
+                                        <div class="money-fl">￥${item.price}</div>
+                                    </div>
+                                    <div class="product-comment">
+                                        <div class="pro-name"><a></a></div>
+                                        <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-left" style="cursor:pointer;">${item.pinjia_num}</div>
+                                        <div class="pro-assess-new" style="cursor:pointer;">${item.dianzanlv}</div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-right">
+                                            <a>${item.business_category}</a>
                                         </div>
                                     </div>
-                                </li>
-                            `;
-                            
-                        }else if(item.pro_icon_while_one == 1){
-                            return `
-                                <li data-id="${item.id}">
-                                    <div class="pro-show">
-                                        <div class="pro-icon"><img src="../img/icon-bl-2.png" alt="" /></div>
-                                        <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
-                                        <div class="pro-money">
-                                            <div class="money-fl">￥${item.price}</div>
-                                        </div>
-                                        <div class="product-comment">
-                                            <div class="pro-name"><a></a></div>
-                                            <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-left" style="cursor:pointer;">${item.pinjia_num}</div>
-                                            <div class="pro-assess-new" style="cursor:pointer;">${item.dianzanlv}</div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-right">
-                                                <a>${item.business_category}</a>
-                                            </div>
-                                        </div>
-                                        <div class="pro-button">
-                                            <button class="btn btn-primary addCard">加入购物车</button>
-                                        </div>
+                                    <div class="pro-button">
+                                        <button class="btn btn-primary addCard">加入购物车</button>
                                     </div>
-                                </li>
-                            `;
-                        }
-                    }else if(item.ishot == 1){
-                        if(item.pro_icon_while_one == 0){
-                            return `
-                                <li data-id="${item.id}">
-                                    <div class="pro-show">
-                                        <div class="pro-icon"><img src="../img/icon-bl-3.png" alt="" /></div>
-                                        <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
-                                        <div class="pro-money">
-                                            <div class="money-fl">￥${item.price}</div>
-                                        </div>
-                                        <div class="product-comment">
-                                            <div class="pro-name"><a></a></div>
-                                            <div class="pro-discount"><a>${item.details}</a></div>
-                                        </div>
-                                        <div class="pro-top">
-                                            <div class="pro-top-left">
-                                                <span>在</span>
-                                                <a class="red">&nbsp;${item.category}&nbsp;</a>
-                                                <span>中</span>
-                                                <span class="red">畅销</span>
-                                            </div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-right">
-                                                <a>${item.business_category}</a>
-                                            </div>
-                                        </div>
-                                        <div class="pro-button">
-                                            <button class="btn btn-primary addCard">加入购物车</button>
-                                        </div>
-                                    </div>
-                                </li>
-                            `;
-                        }else if(item.pro_icon_while_one == 1){
-                            return `
-                                <li data-id="${item.id}">
-                                    <div class="pro-show">
-                                        <div class="pro-icon"><img src="../img/icon-bl-2.png" alt="" /></div>
-                                        <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
-                                        <div class="pro-money">
-                                            <div class="money-fl">￥${item.price}</div>
-                                        </div>
-                                        <div class="product-comment">
-                                            <div class="pro-name"><a></a></div>
-                                            <div class="pro-discount"><a>${item.details}</a></div>
-                                        </div>
-                                        <div class="pro-top">
-                                            <div class="pro-top-left">
-                                                <span>在</span>
-                                                <a class="red">&nbsp;${item.category}&nbsp;</a>
-                                                <span>中</span>
-                                                <span class="red">畅销</span>
-                                            </div>
-                                        </div>
-                                        <div class="pro-assess">
-                                            <div class="pro-assess-right">
-                                                <a>${item.business_category}</a>
-                                            </div>
-                                        </div>
-                                        <div class="pro-button">
-                                            <button class="btn btn-primary addCard">加入购物车</button>
-                                        </div>
-                                    </div>
-                                </li>
-                            `;
-                        }
+                                </div>
+                            </li>
+                        `;
                         
+                    }else if(item.pro_icon_while_one == 1){
+                        return `
+                            <li data-id="${item.id}">
+                                <div class="pro-show">
+                                    <div class="pro-icon"><img src="../img/icon-bl-2.png" alt="" /></div>
+                                    <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
+                                    <div class="pro-money">
+                                        <div class="money-fl">￥${item.price}</div>
+                                    </div>
+                                    <div class="product-comment">
+                                        <div class="pro-name"><a></a></div>
+                                        <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-left" style="cursor:pointer;">${item.pinjia_num}</div>
+                                        <div class="pro-assess-new" style="cursor:pointer;">${item.dianzanlv}</div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-right">
+                                            <a>${item.business_category}</a>
+                                        </div>
+                                    </div>
+                                    <div class="pro-button">
+                                        <button class="btn btn-primary addCard">加入购物车</button>
+                                    </div>
+                                </div>
+                            </li>
+                        `;
                     }
+                }else if(item.ishot == 1){
+                    if(item.pro_icon_while_one == 0){
+                        return `
+                            <li data-id="${item.id}">
+                                <div class="pro-show">
+                                    <div class="pro-icon"><img src="../img/icon-bl-3.png" alt="" /></div>
+                                    <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
+                                    <div class="pro-money">
+                                        <div class="money-fl">￥${item.price}</div>
+                                    </div>
+                                    <div class="product-comment">
+                                        <div class="pro-name"><a></a></div>
+                                        <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
+                                    </div>
+                                    <div class="pro-top">
+                                        <div class="pro-top-left">
+                                            <span>在</span>
+                                            <a class="red">&nbsp;${item.category}&nbsp;</a>
+                                            <span>中</span>
+                                            <span class="red">畅销</span>
+                                        </div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-right">
+                                            <a>${item.business_category}</a>
+                                        </div>
+                                    </div>
+                                    <div class="pro-button">
+                                        <button class="btn btn-primary addCard">加入购物车</button>
+                                    </div>
+                                </div>
+                            </li>
+                        `;
+                    }else if(item.pro_icon_while_one == 1){
+                        return `
+                            <li data-id="${item.id}">
+                                <div class="pro-show">
+                                    <div class="pro-icon"><img src="../img/icon-bl-2.png" alt="" /></div>
+                                    <div class="pro-img"><a href="http://localhost:1111/html/goods.html?id=${item.id}"><img src="${item.imgurl}" alt="" /></a></div>
+                                    <div class="pro-money">
+                                        <div class="money-fl">￥${item.price}</div>
+                                    </div>
+                                    <div class="product-comment">
+                                        <div class="pro-name"><a></a></div>
+                                        <div class="pro-discount"><a href="http://localhost:1111/html/goods.html?id=${item.id}">${item.details}</a></div>
+                                    </div>
+                                    <div class="pro-top">
+                                        <div class="pro-top-left">
+                                            <span>在</span>
+                                            <a class="red">&nbsp;${item.category}&nbsp;</a>
+                                            <span>中</span>
+                                            <span class="red">畅销</span>
+                                        </div>
+                                    </div>
+                                    <div class="pro-assess">
+                                        <div class="pro-assess-right">
+                                            <a>${item.business_category}</a>
+                                        </div>
+                                    </div>
+                                    <div class="pro-button">
+                                        <button class="btn btn-primary addCard">加入购物车</button>
+                                    </div>
+                                </div>
+                            </li>
+                        `;
+                    }
+                    
+                }
 
-                }).join('');
-                prolist.appendChild(ul);
+            }).join('');
+            prolist.innerHTML = '';
+            prolist.appendChild(ul);
 
+            // 生成页码
+            var ul = $('<ul/>').addClass('page').get(0);
+            ul.innerHTML = '';
+            for(var i=0;i<data.total_page;i++){
+                var li = document.createElement('li');
+                var a = document.createElement('a');
+                a.innerText = i+1;
+                if(i+1 == pageNo){
+                    li.className = 'on';
+                }
+                li.appendChild(a);
+                ul.appendChild(li);
             }
-        });
+            $('.page-show').get(0).innerHTML = '';
+            $('.page-show').get(0).appendChild(ul);
+        }
+
+    }
+
+
+    xhr.open('get',`http://localhost:1111/api/list.php?pageNo=${pageNo}&qty=${qty}`,true);
+    xhr.send();
+
+    // 分页切换
+    $('.page-show').on('click','a',function(){
+        pageNo = $(this).text();
+        xhr.open('get',`http://localhost:1111/api/list.php?pageNo=${pageNo}&qty=${qty}`,true);
+        xhr.send();
+    });
+    
+
         
 });
